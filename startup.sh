006ad39b90df531409860e61d2a3e14fd9f4f079
AbrahamBribi <abribiesca@itexico.net>
2017-02-09 09:50:53 -0600

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 export DISPLAY=:1
 Xvfb :1 -screen 0 1600x900x16 &
-sleep 5
-openbox-session&
-#x11vnc -display :1 -nopw -listen localhost -xkb -ncache 10 -ncache_cr -forever &
+sleep 7
+lxsession&
 x11vnc -forever -nopw -httpdir /usr/share/vnc-java/ -httpport 5800 -shared &
-cd /root/noVNC && ln -s vnc_auto.html index.html && ./utils/launch.sh --vnc localhost:5900
+cd /root/noVNC && ln -s vnc_auto.html index.html && ./utils/launch.sh --vnc localhost:5900 &
