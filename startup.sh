f5a623ec2388ce5a6606608cf885605aafe87ee0
AbrahamBribi <abribiesca@itexico.net>
2017-02-09 13:50:02 -0600

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -4,4 +4,4 @@ Xvfb :1 -screen 0 1600x900x16 &
 sleep 7
 startlxde &
 x11vnc -forever -nopw -httpdir /usr/share/vnc-java/ -httpport 5800 -shared &
-cd /root/noVNC && ln -s vnc_auto.html index.html && ./utils/launch.sh --vnc localhost:5900 &
+cd /root/noVNC && ln -s vnc_auto.html index.html && ./utils/launch.sh --vnc localhost:5900 
